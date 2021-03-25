7b95f1085906d5aabdb8b1f5d9523cfe8cf29d8d
Silja18 <silja.baumann@student.uibk.ac.at>
2021-03-25 17:25:19 +0100

    Versucht marker einzufügen

--- nz/main.js
+++ nz/main.js
@@ -19,9 +19,17 @@ const map = L.map("map", {
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
     ]
 });
+console.log (ROUTE);
+for (let entry of ROUTE) {
+    console.log(entry);
+    let mrk = L.marker([-44.483333, 169.966667]) .addTo(map);
+mrk.bindPopup(`
+<h4>Stop ${entry.nr}: ${entry.name}<h4>
+<p><i class="fas fa-external-link-alt"></i> <a href="${entry.wikipedia}">Read about stop in Wikipedia</a></p>
+`).openPopup();
+}
 
-let mrk = L.marker([-44.483333, 169.966667]) .addTo(map);
-mrk.bindPopup("Omarama").openPopup();
+
 
 //WMTS-Services werden für die Kacheln verwendet
 //consle.log(document,querySelector("#map")); um die Konsole zu checken
