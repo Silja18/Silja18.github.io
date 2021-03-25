b3cc186fe4e4a6516c4cb3afb514e1b20f1a2158
Silja18 <silja.baumann@student.uibk.ac.at>
2021-03-25 16:17:23 +0100

    Karte erstellt mit center zoom und layer

--- nz/main.js
+++ nz/main.js
@@ -1,6 +1,10 @@
 
 const map = L.map("map", {
-    center: [ -44.483333, 169.966667 ]
+    center: [ -44.483333, 169.966667 ], 
+    zoom: 13,
+    layers: [
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+    ]
 });
-
+//WMTS-Services werden für die Kacheln verwendet
 console.log(document.querySelector("#map"));
