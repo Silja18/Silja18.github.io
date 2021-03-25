564816cdd5a917706fc91b17296be57cdbbab3c6
Silja18 <silja.baumann@student.uibk.ac.at>
2021-03-25 16:34:45 +0100

    stop Objekt erzeugt und verwendet

--- nz/main.js
+++ nz/main.js
@@ -1,6 +1,19 @@
 
+let stop = {
+    nr: 4,
+    name: "Omarama",
+    lat: -44.483333,
+    lng: 169.966667,
+    user: "Silja18",
+    wikipedia: "https://en.wikipedia.org/wiki/Omarama"
+};
+/*console.log(stop);
+console.log(stop.name);
+console.log(stop.lat);
+console.log(stop.lng);
+console.log(wikipedia); */
 const map = L.map("map", {
-    center: [ -44.483333, 169.966667 ], 
+    center: [ stop.lat, stop.lng ], 
     zoom: 13,
     layers: [
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
@@ -9,5 +22,7 @@ const map = L.map("map", {
 
 let mrk = L.marker([-44.483333, 169.966667]) .addTo(map);
 mrk.bindPopup("Omarama").openPopup();
+
 //WMTS-Services werden für die Kacheln verwendet
+//consle.log(document,querySelector("#map")); um die Konsole zu checken
 console.log(document.querySelector("#map"));
