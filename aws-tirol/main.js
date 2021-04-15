7ebd1921010dcd0ecece7ebf256f378ce3270152
Silja18 <silja.baumann@student.uibk.ac.at>
2021-04-15 17:44:53 +0200

    Java Code neu eingefügt

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -11,14 +11,13 @@ let map = L.map("map", {
 let layerControl = L.control.layers({
     "BasemapAT.grau": basemapGray,
     "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto'),
-    "BasemapAT.terrain": L.tileLayer.provider('BasemapAT.terrain'),
-    "BasemapAT.overlay": L.tileLayer.provider('BasemapAT.overlay'),
-    "BasemapAT.highdpi": L.tileLayer.provider('BasemapAT.highdpi'),
+    "BasemapAT.surface": L.tileLayer.provider('BasemapAT.surface'),
     "BasemapAT.overlay+ortho": L.layerGroup([
         L.tileLayer.provider('BasemapAT.orthofoto'),
-        L.tileLayer.provider('Basemap.AT.overlay'),
+        L.tileLayer.provider('BasemapAT.overlay')
     ])
 }).addTo(map);
+
 
 let awsUrl = 'https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson';
 
@@ -31,23 +30,26 @@ fetch(awsUrl)
     .then(json => {
         console.log('Daten konvertiert: ', json);
         for (station of json.features) {
-            console.log('Station: ', station);
-            let marker = L.marker(
-                [station.geometry.coordinates[1],
+            // console.log('Station: ', station);
+            let marker = L.marker([
+                station.geometry.coordinates[1],
                 station.geometry.coordinates[0]
             ]);
             let formattedDate = new Date(station.properties.date);
-            marker.bindPopup(`<h3>${station.properties.name}</h3>
+            marker.bindPopup(`
+            <h3>${station.properties.name}</h3>
             <ul>
-                <li> Datum: ${formattedDate.toLocaleString ("de")}</li>
-                <li> Temperatur: ${station.properties.LT} C</li>
-                <li> Schneehöhe: ${station.properties.LT} </li>
-                <li> Luftfeuchtigkeit: ${station.properties.LT} </li>
-                <li> Seehöhe: ${station.properties.LT} </li>
+              <li>Datum: ${formattedDate.toLocaleString("de")}</li>
+              <li>Seehöhe: ${station.geometry.coordinates[2]} m</li>
+              <li>Temperatur: ${station.properties.LT} C</li>
+              <li>Schneehöhe: ${station.properties.HS || '?'} cm</li>
+              <li>Windgeschwindigkeit: ${station.properties.WG || '?'} km/h</li>
+              <li>Windgeschwindrichtung: ${station.properties.WR || '?'}</li>
             </ul>
+            <a target="_blank" href="https://wiski.tirol.gv.at/lawine/grafiken/1100/standard/tag/${station.properties.plot}.png">Grafik</a>
             `);
             marker.addTo(awsLayer);
         }
-        // see map view to all settings
+        // set map view to all stations
         map.fitBounds(awsLayer.getBounds());
     });
