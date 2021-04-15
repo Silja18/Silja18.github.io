b6ceeaa290390c7e372672ffe73f877eefc68588
Silja18 <silja.baumann@student.uibk.ac.at>
2021-04-15 17:56:26 +0200

    highlight class für Schnee höhen eingefügt und in css

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -23,7 +23,11 @@ let awsUrl = 'https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson';
 
 let awsLayer = L.featureGroup();
 layerControl.addOverlay(awsLayer, "Wetterstationen Tirol");
-awsLayer.addTo(map);
+// awsLayer.addTo(map);
+let snowLayer = L.featureGroup();
+layerControl.addOverlay(snowLayer, "Schneehöhen");
+snowLayer.addTo(map);
+
 
 fetch(awsUrl)
     .then(response => response.json())
@@ -49,6 +53,26 @@ fetch(awsUrl)
             <a target="_blank" href="https://wiski.tirol.gv.at/lawine/grafiken/1100/standard/tag/${station.properties.plot}.png">Grafik</a>
             `);
             marker.addTo(awsLayer);
+            if (station.properties.HS) {
+                let highlightClass = '';
+                if (station.properties.HS > 100) {
+                    highlightClass = 'snow-100';
+                }
+                if (station.properties.HS > 200) {
+                    highlightClass = 'snow-200';
+                }
+                
+                let snowIcon = L.divIcon({
+                    html: `<div class="snow-label">${station.properties.HS}</div>`
+                })
+                let snowMarker = L.marker([
+                    station.geometry.coordinates[1],
+                    station.geometry.coordinates[0]
+                ], {
+                    icon: snowIcon
+                });
+                snowMarker.addTo(snowLayer);
+            }
         }
         // set map view to all stations
         map.fitBounds(awsLayer.getBounds());
