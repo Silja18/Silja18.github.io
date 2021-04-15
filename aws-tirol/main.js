ede39813cb2df84cba8343115d2101e1ecbf0a60
Silja18 <silja.baumann@student.uibk.ac.at>
2021-04-15 16:08:52 +0200

    neue Codes reinkopiert

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -1,10 +1,17 @@
+let basemapGray = L.tileLayer.provider('BasemapAT.grau');
 
-let basemapGrey = L.tileLayer.provider(' BasemapAT.grau');
-
-let map = L.map ("map"), {
-    center: [47, 11]
+let map = L.map("map", {
+    center: [47, 11],
     zoom: 9,
     layers: [
-        basemapGrey
+        basemapGray
     ]
 });
+
+let layerControl = L.control.layers({
+    "BasemapAT.grau": basemapGray,
+    "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto'),
+    "BasemapAT.terrain": L.tileLayer.provider('BasemapAT.terrain'),
+    "BasemapAT.overlay": L.tileLayer.provider(' BasemapAT.overlay'),
+    "BasemapAT.highdpi": L.tileLayer.provider('BasemapAT.highdpi'),
+}).addTo(map);
