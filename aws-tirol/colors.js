7abd0724e318deca6cb4643f34cc968635a0db80
Silja18 <silja.baumann@student.uibk.ac.at>
2021-04-21 11:05:50 +0200

    Farben für Schnee eingefügt, sehe aber nicht die Höhen...

---
+++ aws-tirol/colors.js
@@ -0,0 +1,68 @@
+const COLORS = {
+    windspeed: [{
+        min: 0,
+        max: 5,
+        col: "#ffff64"
+    }, {
+        min: 5,
+        max: 10,
+        col: "#c8ff64"
+    }, {
+        min: 10,
+        max: 20,
+        col: "#96ff96"
+    }, {
+        min: 20,
+        max: 40,
+        col: "#32c8ff"
+    }, {
+        min: 40,
+        max: 60,
+        col: "#6496ff"
+    }, {
+        min: 60,
+        max: 80,
+        col: "#9664ff"
+    }, {
+        min: 80,
+        max: 999,
+        col: "#ff3232"
+    }],
+    temperature: [
+        
+    ],
+    snowheight: [{
+        min: 0,
+        max: 10,
+        col: "#ffffb2",
+    }, {
+        min: 
+        max:
+        col: "#b0ffbc",
+    }, {
+        min:
+        max:
+        col: "#8cffff",
+    }, {
+        min:
+        max:
+        col: "#19cdff",
+    }, {
+        min:
+        max:
+        col: "#1982ff",
+    }, {
+        min: 
+        max:
+        col: "#0f5abe",
+    }, {
+        min:
+        max:
+        col: "#784bff",
+    }, {
+        min: 
+        max 
+        col: "#cd0feb",
+    },     
+    ]
+};
