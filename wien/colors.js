bd5b3372c82f0880a0e4a112036562b2df3c7607
Silja18 <silja.baumann@student.uibk.ac.at>
2021-05-06 17:58:03 +0200

    Farben für Minuten aus colorbrewer eingefügt

--- wien/colors.js
+++ wien/colors.js
@@ -6,5 +6,14 @@ const COLORS = {
         "Orange Line": "#FF851B",
         "Red Line": "#FF4136",
         "Yellow Line": "#FFDC00"
+    },
+    minutes: {
+        "5": "#d73027",
+        "10": "#fc8d59",
+        "15": "#fee08b",
+        "20": "#d9ef8b",
+        "25": "#91cf60",
+        "30": "#1a9850",
     }
+
 };
