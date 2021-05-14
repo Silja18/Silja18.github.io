3249eade6c27371f765a8933359a5baa711ff14c
Silja18 <silja.baumann@student.uibk.ac.at>
2021-05-14 16:51:44 +0200

    Fullscreen in main.js implementiert

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -2,6 +2,7 @@ let basemapGray = L.tileLayer.provider('BasemapAT.grau');
 // https://leafletjs.com/reference-1.7.1.html#tilelayer
 let map = L.map("map", {
     // https://leafletjs.com/reference-1.7.1.html#map-example
+    fullscreenControl: true,
     center: [47, 11],
     zoom: 9,
     layers: [
