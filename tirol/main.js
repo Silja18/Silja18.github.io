06f79b7581e5b2ed45ea56a366c38ce9103691ce
Silja18 <silja.baumann@student.uibk.ac.at>
2021-05-27 17:47:54 +0200

    Streckenbeschreibung eingebaut

--- tirol/main.js
+++ tirol/main.js
@@ -175,12 +175,13 @@ const updateTexts = (nr) => {
        // console.log(etappe);
         // ist es die aktuelle Etappe
         if (etappe.nr == nr) {
-            console.log("unsere Etappe", etappe);
+            // console.log("unsere Etappe", etappe);
+            etappe.homepage = `<a href="${etappe.weblink}">Homepage</a>`;
             for (let key in etappe) {
                 console.log("key:", key, "value:", etappe[key]);
                 // gibt es ein Element in HTML mit der ID von "key"?
                 if (document.querySelector(`#text-${key}`)) {
-                   // console.log("Juhu", key, etappe[key]);
+                    //console.log("Juhu", key, etappe[key]);
                     document.querySelector(`#text-${key}`).innerHTML = etappe[key];
                 }
             }
